79c79b8751503459b81f6e699cde0ab9cd937e7d
Yadnia <Baltoyadnia@gmail.com>
2023-09-26 10:15:38 -0600

    add .

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,88 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/carrito', function () {
+    return view('carrito');
+});
+
+Route::get('/perfil', function () {
+    return view('perfil');
+});
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+Route::get('/tiendas', function () {
+    return view('tiendas');
+});
+
+Route::get('/tienda-ejemplo', function () {
+    return view('tienda-ejemplo');
+});
+
+Route::get('/historial', function () {
+    return view('/historial');
+});
+Route::get('/mensaje', function () {
+    return view('/mensaje');
+});
+
+Route::get('/mensaje-ejemplo', function () {
+    return view('/mensaje-ejemplo');
+});
+Route::get('/prodhist', function () {
+    return view('/prodhist');
+});
+Route::get('/tendencias', function () {
+    return view('/tendencias');
+});
+Route::get('/productoten', function () {
+    return view('/productoten');
+});
+Route::get('/listadeseos', function () {
+    return view('/listadeseos');
+});
+Route::get('/productodeseos', function () {
+    return view('/productodeseos');
+
+});
+Route::get('/podriangustar', function () {
+    return view('/podriangustar');
+});
+
+Route::get('/prodpodrian', function () {
+    return view('/prodpodrian');
+});
+
+Route::get('/categorias', function () {
+    return view('/categorias');
+});
+
+Route::get('/arte', function () {
+    return view('/arte');
+});
+
+Route::get('/artesania', function () {
+    return view('/artesania');
+});
+
+Route::get('/manual', function () {
+    return view('/manual');
+});
+
+
+Route::get('/prodartesania', function () {
+    return view('/prodartesania');
+});
+Route::get('/prodarte', function () {
+    return view('/prodarte');
+});
+Route::get('/prodmanual', function () {
+    return view('/manual');
+});
+Route::get('/prodcarrito', function () {
+    return view('/prodcarrito');
+});
 
 Auth::routes();
 
